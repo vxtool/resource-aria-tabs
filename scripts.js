477d54a4f4e8ee14d747176fa680341ef5317262
Hemerson Vianna <hemerson.lourenco@gmail.com>
2015-12-13 12:48:01 -0200

    method of addEventListeners

--- scripts.js
+++ scripts.js
@@ -7,8 +7,22 @@
 		this.addEvents();
 	};
 
-	
+	Tabs.prototype.addEventListeners = function( elements, event, method ) {
+		var that 	= this;
+		var _this 	= {};
 
+		[].forEach.call(elements, function(element, index){
+			_this = {
+				that: that,
+				index: index,
+				element: element
+			}
+			
+			element.addEventListener( event, method.bind(_this) );
+		});
+	};
+
+	
 })();
 
 
