1df11d0645df04c1f0aa044074bc07fe2fed67c0
Hemerson Vianna <hemerson.lourenco@gmail.com>
2015-12-13 12:46:58 -0200

    added script

---
+++ scripts.js
@@ -0,0 +1,14 @@
+(function(){
+
+	var Tabs = function(){
+		this.$container = document.querySelector('.tabs');
+		this.$elements 	= this.$container.querySelectorAll('.tabs-link');
+		
+		this.addEvents();
+	};
+
+	
+
+})();
+
+
