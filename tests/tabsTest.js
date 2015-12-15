3ecc136ce761d2b4ef3bdae5c38cf305008866f8
Hemerson Vianna <hemerson.lourenco@gmail.com>
2015-12-15 01:58:14 -0200

    added example of test

---
+++ tests/tabsTest.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+
+describe('Alguns testes de exemplo', function(){
+	
+	it('2 + 2 deve ser igual a 4', function(){
+		assert.equal(4, 2 + 2);
+	});
+
+	it('2 * 2 deve ser igual a 8 (nota 0 em matemática)', function(){
+		assert.equal(8, 2 * 2);
+	});
+
+});
