bc1b3496bd9f7a2d1396ecb9a680bf54f7d0a35c
Hemerson Vianna <hemerson.lourenco@gmail.com>
2015-12-16 12:00:09 -0200

    added tests

--- test/tabsTest.js
+++ test/tabsTest.js
@@ -5,16 +5,48 @@ describe('Tabs', function(){
 
 	jsdom();
 
+	var $tabs;
+	var $tabsLink;
+	var $tabsContent;
+	var dom = '<div class="tabs">';
+		dom +='<a class="tabs-link" aria-selected="true" tabindex="0" href="#content1">Link 1</a>';
+		dom +='<a class="tabs-link" tabindex="-1" href="#content2">Link 2</a>';
+		dom +='<section class="tabs-content" id="content1"></section>';
+		dom +='<section class="tabs-content" id="content2" aria-hidden="true"></section>';
+		dom +='</div>';
+
 	before(function() {
-		document.body.innerHTML = '<div class="tabs"><a class="tabs-link" href="#content1">Link 1</a><section id="content1"></section></div>';
+		
+		document.body.innerHTML = dom;
+
+		$tabs 			= document.querySelectorAll('.tabs');
+		$tabsLink 		= document.querySelectorAll('.tabs-link');
+		$tabsContent 	= document.querySelectorAll('.tabs-content');
 	});
 
-	it('Tabs exists', function () {
-		assert.equal(document.querySelectorAll('.tabs').length, 1);
+	it('There are elements', function () {
+		assert.equal($tabs.length, 1);
+		assert.equal($tabsLink.length, 2);
+		assert.equal($tabsContent.length, 2);
 	});
 
-	it('Links exists', function () {
-		assert.equal(document.querySelectorAll('.tabs-link').length, 1);
+	it('Item selected', function () {
+		assert.equal($tabsLink[0].getAttribute('aria-selected'), 'true');
+		assert.equal($tabsLink[0].getAttribute('tabindex'), '0');
+
+		assert.equal($tabsContent[0].getAttribute('aria-hidden'), null);
+		
+		assert.equal($tabsLink[0].getAttribute('href').substring(1), $tabsContent[0].getAttribute('id'));
 	});
+
+	it('Item is not selected', function () {
+		assert.equal($tabsLink[1].getAttribute('aria-selected'), null);
+		assert.equal($tabsLink[1].getAttribute('tabindex'), '-1');
+
+		assert.equal($tabsContent[1].getAttribute('aria-hidden'), 'true');
+
+		assert.equal($tabsLink[1].getAttribute('href').substring(1), $tabsContent[1].getAttribute('id'));
+	});
+
 
 });
